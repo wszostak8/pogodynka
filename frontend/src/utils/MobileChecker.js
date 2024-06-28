cf062537392a55dd1a047fdd782bdc5f6debe593
Wiktor Szostak <wiktorszostak6@gmail.com>
2024-06-28 21:05:13 +0200

    update

--- frontend/src/utils/MobileChecker.js
+++
@@ -1,9 +0,0 @@
-export const Mobile = ({ children }) => {
-    const isMobile = window.innerWidth <= 800; // Przykładowa wartość, możesz dostosować według potrzeb
-    return isMobile ? children : null;
-};
-
-export const Desktop = ({ children }) => {
-    const isDesktop = window.innerWidth > 1024; // Przykładowa wartość, możesz dostosować według potrzeb
-    return isDesktop ? children : null;
-};
